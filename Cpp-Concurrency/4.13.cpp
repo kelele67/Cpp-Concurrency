0c3aa6c21a3a03d60aa4c2d0caa10964217f1bf4
kelele67 <kingand67@outlook.com>
2017-07-18 16:30:56 +0800

    快速排序一一“期望”并行版

--- Cpp-Concurrency/4.13.cpp
+++ Cpp-Concurrency/4.13.cpp
@@ -22,7 +22,7 @@ std::list<T> parallel_quick_sort(std::list<T> input) {
     lower_part.splice(lower_part.end(), input, input.begin(), divide_point);
     // 不对小于中间值的部分排序，使用std::async在另一线程对其进行排序
     std::future<std::list<T> > new_lower(std::async(&parallel_quick_sort<T>, std::move(lower_part)));
-    // 大于部分如同之前一样，使用递归排序， 递归两次，4个线程；三次，8个线程...
+    // 大于部分如同之前一样，使用递归排序，递归两次，4个线程；三次，8个线程...
     auto new_higher(parallel_quick_sort(std::move(input)));
     result.splice(result.end(), new_higher);
     result.splice(result.begin(), new_higher.get());
