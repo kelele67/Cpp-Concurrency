5f0b1faed7c4d83dc536eaaed7284609ccb102f5
kelele67 <kingand67@outlook.com>
2017-07-18 09:55:46 +0800

    使用 boost::shared_mutex 对数据结构进行保护->简单的DNS缓存

---
+++ Cpp-Concurrency/3.13.cpp
@@ -0,0 +1,9 @@
+//
+//  3.13.cpp 保护很少更新的数据结构
+//  Cpp-Concurrency
+//
+//  Created by 刘启 on 2017/7/18.
+//  Copyright © 2017年 kelele67. All rights reserved.
+//  使用 boost::shared_mutex 对数据结构进行保护->简单的DNS缓存
+
+#include <stdio.h>
