bc57e775c50e2b0c4e0a902a0d68d74dcd81c38d
kelele67 <kingand67@outlook.com>
2017-07-18 18:03:29 +0800

    不同线程对数据的读写

--- Cpp-Concurrency/5.2.cpp
+++ Cpp-Concurrency/5.2.cpp
@@ -5,6 +5,7 @@
 //  Created by 刘启 on 2017/7/18.
 //  Copyright © 2017年 kelele67. All rights reserved.
 //  不同线程对数据的读写
+//  对于非原子操作，使用原子操作对操作进行强制排序
 
 #include <vector>
 #include <atomic>
@@ -16,13 +17,13 @@ std::vector<int> data;
 std::atomic_bool data_ready(false);
 
 void reader_thread() {
-    while (!data_ready.load()) {
+    while (!data_ready.load()) { // 等待数据
         std::this_thread::sleep_for(std::chrono::milliseconds(1));
     }
-    std::cout << "The answer=" << data[0] << "\n";
+    std::cout << "The answer=" << data[0] << "\n"; // 非原子读出数据
 }
 
 void writer_thread() {
-    data.push_back(42);
+    data.push_back(42); // 非原子写入数据
     data_ready = true;
 }
