453954339a2996bf37182ddc84afb6f05f500de2
kelele67 <kingand67@outlook.com>
2017-07-20 09:42:19 +0800

    不用锁实现栈的push()

--- Cpp-Concurrency/7.1.cpp
+++ Cpp-Concurrency/7.1.cpp
@@ -27,6 +27,8 @@ public:
     void push(T const& data) {
         node* const new_node = new node(data); // 1.创建一个新节点
         new_node->next = head.load(); // 2.将当前节点的next指针指向当前head的节点
+        
+        // compare_exchange_weak->相同，返回false，说明更新了栈顶；不同，则交换两个值，则让head指针指向了新节点
         // compare_exchange_weak如果失败，则栈顶被更新了，这个时候更新的新栈顶会被更新到new_node->next，因此循环可以再次尝试压栈而无需手动更新new_node->next
         // ->保证在步骤3的时候栈顶和步骤2中获得的栈顶相同，如果不同，就自旋->重新获取栈顶
         while (!head.compare_exchange_weak(new_node->next, new_node)); // 3.让head指针指向新节点
